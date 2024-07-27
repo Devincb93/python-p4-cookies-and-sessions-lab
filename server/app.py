203c0aaa09b924f0f9411a41b7f914aeb7efb410
Devin <devincb93@gmail.com>
2024-07-26 17:14:42 -0700

    complete

--- server/app.py
+++ server/app.py
@@ -20,15 +20,39 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
-@app.route('/articles')
+@app.route('/articles', methods = ['GET'])
 def index_articles():
+    articles = Article.query.all()
 
-    pass
+    articles_dict = articles.to_dict()
+    return make_response(
+        articles_dict, 200
+    )
 
-@app.route('/articles/<int:id>')
+@app.route('/articles/<int:id>', methods = ['GET'])
 def show_article(id):
+    session['page_views'] = 1 if 'page_views' not in session else session['page_views'] + 1
+    if session['page_views'] <= 3:
+        # Retrieve the article from the database
+        article = Article.query.filter(Article.id ==id).first()
+        
+        if article:
+            return jsonify({
+                'id': article.id,
+                'author': article.author,
+                'title': article.title,
+                'content': article.content,
+                'preview': article.preview,
+                'minutes_to_read': article.minutes_to_read,
+                'date': article.date,
+                'page_views': session['page_views']
+            })
+        else:
+            return {'message': 'Article not found'}, 404
+    else:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
-    pass
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
